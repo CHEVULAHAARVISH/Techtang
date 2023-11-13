4bdeb87958bd14fcb22eb1958b4949fa58c4a377
Haarvish <haarvish@gmail.com>
2023-11-13 20:31:48 +0530

    Deployment

--- utils/database.js
+++ utils/database.js
@@ -7,7 +7,7 @@ export const connectToDb = async ()=>{
         return;
     }
     try {
-        await mongoose.connect(process.env.MONGODB_URL,{
+        await mongoose.connect(process.env.MONGODB_URI,{
             dbName:"TechPost",
             useNewUrlParser:true,
             useUnifiedTopology:true,
